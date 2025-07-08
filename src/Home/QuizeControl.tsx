e96845997fc4e5dea9d84dda9b88090ed1a2a959
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-08 09:56:52 +0300

    some state added in code

--- src/Home/QuizeControl.tsx
+++ src/Home/QuizeControl.tsx
@@ -1,15 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { nextQuestion, previusQution } from "@/redux/features/quizSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 export default function QuizeControl() {
+  const {  currentQuestionIndex, userAnswer } = useAppSelector(
+    // questions,
+    (state) => state.quiz
+  );
+  const dispatch = useAppDispatch();
+  const isAnswerSelected = userAnswer[currentQuestionIndex] !== null;
 
-    const handelNext=()=>{
-        console.log('btn cline')
-
+  const handelNext = () => {
+    if (isAnswerSelected) {
+      dispatch(nextQuestion());
     }
-  return <div className="flex justify-between mt-3 space-x-4">
-
-
-    <Button>previous</Button>
-    <Button onClick={handelNext}>next</Button>
-    </div>;
+  };
+  const handelPrev = () => {
+    dispatch(previusQution());
+  };
+  return (
+    <div className="flex justify-between mt-3 space-x-4">
+      <Button onClick={handelPrev}>previous</Button>
+      <Button onClick={handelNext}>next</Button>
+    </div>
+  );
 }
--- src/redux/features/quizSlice.ts
+++ src/redux/features/quizSlice.ts
@@ -2,23 +2,34 @@ import { quizData } from "@/Home/quizData";
 import { createSlice } from "@reduxjs/toolkit";
 // import type { RootState } from "../store";
 
-const initialState  = {
+const initialState = {
   questions: quizData,
-  currentQuestionIndex:0,
-  userAnswer:Array(quizData.length).fill(null)
+  currentQuestionIndex: 0,
+  userAnswer: Array(quizData.length).fill(null),
 };
-console.log( typeof length)
+console.log(typeof length);
 
 export const quizeSclie = createSlice({
   name: "quiz",
-  initialState ,
+  initialState,
   reducers: {
-setAuswer:(state, action)=>{
-const {questionIndex, answer}= action.payload;
-state.userAnswer[questionIndex]= answer
-}
+    setAuswer: (state, action) => {
+      const { questionIndex, answer } = action.payload;
+      state.userAnswer[questionIndex] = answer;
+    },
+    nextQuestion: (state) => {
+      if (state.currentQuestionIndex < state.questions.length - 1) {
+        state.currentQuestionIndex += 1;
+      }
+    },
+    previusQution:(state)=>{
+      if(state.currentQuestionIndex >0){
+        state.currentQuestionIndex -= 1;
+      }
+
+    }
   },
 });
-export const { setAuswer } = quizeSclie.actions;
+export const { setAuswer, nextQuestion,previusQution } = quizeSclie.actions;
 
 export default quizeSclie.reducer;
